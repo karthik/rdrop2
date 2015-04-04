ba6811728caa36ab19d7d626cb5c98f4f5361686
Karthik Ram <karthik.ram@gmail.com>
2015-04-04 12:30:46 -0700

    Started adding tests
    - Need to fortify a set of keys for a test dropbox account to work on Travis

--- tests/rdrop2_tests.R
+++ tests/rdrop2_tests.R
@@ -1,1 +1,9 @@
+options("httr_oauth_cache" = TRUE)
 
+context("Test that basic file ops work correctly") {
+    write.csv(mtcars, "mtcars.csv")
+    # Check to see if file uploads are successful
+    expect_message(drop_upload("mtcars.csv"), "successfully")
+    expect_message(drop_delete("mtcars.csv"), "successfully deleted")
+    unlink("mtcars.csv")
+}
