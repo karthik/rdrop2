539ec2546e921149cb3f698706c790193d7f99b9
Karthik Ram <karthik.ram@gmail.com>
2015-04-04 13:04:57 -0700

    Added more tests

--- tests/rdrop2_tests.R
+++ tests/rdrop2_tests.R
@@ -1,9 +1,18 @@
 options("httr_oauth_cache" = TRUE)
 
-context("Test that basic file ops work correctly") {
+context("Testing that acc info works correctly")
+test_that("Account information works correctly", {
+expect_is("data.frame", drop_acc())
+expect_is("list", drop_acc(verbose = TRUE))
+})
+
+
+context("File ops")
+
+test_that("Test that basic file ops work correctly",{
     write.csv(mtcars, "mtcars.csv")
     # Check to see if file uploads are successful
     expect_message(drop_upload("mtcars.csv"), "successfully")
     expect_message(drop_delete("mtcars.csv"), "successfully deleted")
     unlink("mtcars.csv")
-}
+})
