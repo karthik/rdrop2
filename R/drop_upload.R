b53e489d6173c1ca53e1311cf1849e42bf260447
Karthik Ram <karthik.ram@gmail.com>
2015-04-05 15:32:39 -0700

    Expanding docs

--- R/drop_upload.R
+++ R/drop_upload.R
@@ -4,7 +4,8 @@
 #'
 #'@param file Full local path to file.
 #'@param  dest The relative path on Dropbox where the file should get uploaded.
-#'@param overwrite Default is \code{TRUE}
+#'@param overwrite Default behavior (\code{TRUE}) is to overwrite files in the
+#'  destination. Set to \code{FALSE} to prevent this.
 #'@param autorename This value, either true (default) or false, determines what
 #'  happens when there is a conflict. If true, the file being uploaded will be
 #'  automatically renamed to avoid the conflict. (For example, test.txt might be
