86bd4f52c19cad80a41982842cf57433450f46f3
Karthik Ram <karthik.ram@gmail.com>
2015-04-14 11:44:35 -0700

    Removed drop_size for time being

--- R/drop_utils.R
+++ R/drop_utils.R
@@ -191,16 +191,3 @@ pretty_lists <- function(x)
    invisible(x)
 }
 
-#' @noRd
-drop_size <- function(file) {
-    # def sizeof_fmt(num, suffix='B'):
-    # for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
-    #     if abs(num) < 1024.0:
-    #         return "%3.1f%s%s" % (num, unit, suffix)
-    #     num /= 1024.0
-    # return "%.1f%s%s" % (num, 'Yi', suffix)
-    # This is the python algorithm.
-    # via: http://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size
-
-
-}
