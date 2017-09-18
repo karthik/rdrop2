4a7874891db983dedfa7e7e13321a10b243a95d9
Clayton Yochum <claytonjy@gmail.com>
2017-09-17 22:10:06 -0400

    Update drop_acc to use v2 endpoints, and leave output as list.

--- R/drop_acc.R
+++ R/drop_acc.R
@@ -1,24 +1,36 @@
-
-
-#' Dropbox account information
+#' Get information about current Dropbox account.
 #'
-#' Retrieves information about the user's account. Returns the following fields:  \code{referral_link}, \code{display_name},  \code{uid}, \code{locale}, \code{email_verified}, \code{quota_info.datastores}, \code{quota_info.shared}, \code{quota_info.quota}, \code{quota_info.normal}, \code{is_paired}, \code{country}, \code{name_details.familiar_name}, \code{name_details.surname}, \code{name_details.given_name}, \code{email}
+#' Fields returned will vary by account;
+#'
 #' @template token
-#' @template verbose
+#'
+#' @import httr
 #' @export
-#' @examples \dontrun{
-#' drop_acc()
-#' # Select name and UID
-#' drop_acc() %>% select(uid, display_name, quota_info.normal)
-#'}
-drop_acc <- function(dtoken = get_dropbox_token(), verbose = FALSE) {
-  url <- "https://api.dropbox.com/1/account/info"
-  req <- httr::GET(url, config(token = dtoken))
-  res <- content(req)
-  res <- LinearizeNestedList(res) # This flattens nested JSON.
-  if(verbose) {
-    pretty_lists(res)
-  } else {
-    data.frame(t(unlist(res)))
-  }
+#'
+#' @return
+#'   Nested list with elements \code{account_id},
+#'   \code{name} (list), \code{email}, \code{email_verified}, \code{disabled},
+#'   \code{locale}, \code{referral_link}, \code{is_paired}, \code{account_type}
+#'   (list).
+#'
+#'   If available, may also return \code{profile_photo_url},
+#'   \code{country}, \code{team} (list), \code{team_member_id}.
+#'
+#' @references \href{https://www.dropbox.com/developers/documentation/http/documentation#users-get_current_account}{API documentation}
+#'
+#' @examples
+#' \dontrun{
+#'
+#'   acc_info <- drop_acc()
+#'
+#'   # extract display name
+#'   acc_info$name$display_name
+#' }
+drop_acc <- function(dtoken = get_dropbox_token()) {
+
+  url <- "https://api.dropbox.com/2/users/get_current_account"
+
+  # make request and parse response
+  req <- httr::POST(url, httr::config(token = dtoken))
+  httr::content(req)
 }
