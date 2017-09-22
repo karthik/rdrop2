7f0bc7319fab4038b27ce5cf17bac7222dcff407
Karthik Ram <karthik.ram@gmail.com>
2017-09-22 12:43:50 -0700

    Added a helper function to clean files
    This is important if tests fails partway and you’re left with random bits that make a future test fail.

---
+++ tests/testthat/helper-01.R
@@ -0,0 +1,13 @@
+
+
+#' Use this function to clean out your Dropbox in case there are stray files left over from a failed test.
+clean_dropbox <- function(dtoken = get_dropbox_token()) {
+  x <-
+    readline(
+      "WARNING: this will delete everything in your Dropbox account. Do not do this unless this is a test account. Are you sure?? (y/n)"
+    )
+  if (x == "y") {
+    files <- drop_dir()
+    sapply(files$path, drop_delete)
+  }
+}
