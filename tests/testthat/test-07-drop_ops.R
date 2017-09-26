118dde5e47a8f898cb4d030c3abc4ae07297766b
Karthik Ram <karthik.ram@gmail.com>
2017-09-26 14:18:07 -0700

    tests still failing. 😕

--- tests/testthat/test-07-drop_ops.R
+++ tests/testthat/test-07-drop_ops.R
@@ -6,61 +6,55 @@ context("Testing drop copy")
 # while we work through them
 
 expect_that("drop_copy works correctly", {
-
-# Copying files to files only
-# ------------------------
+# # Copying files to files only
+# # ------------------------
 # We need to start with a clean slate
-cfile_name <- traceless("iris-copy.csv")
+cfile_name <- traceless("iris-test-copy.csv")
 # Copy a file to a new name
 write.csv(iris, cfile_name)
 drop_upload(cfile_name)
-num_copy_files <- drop_dir()  %>% select(name, contains("iris-copy")) %>% nrow
+num_copy_files <- drop_file_count(cfile_name)
 expect_equal(num_copy_files, 1)
 # Copy to a new name, same folder
-cfile_name2 <- traceless("iris-copy.csv")
+cfile_name2 <- traceless("iris-test-copy.csv")
 drop_copy(cfile_name, cfile_name2)
-num_copy_files2 <- drop_dir()  %>% select(name, contains("iris-copy")) %>% nrow
+num_copy_files2 <- drop_file_count("iris-test-copy")
 expect_equal(num_copy_files2, 2)
 exp_2 <- sort(c(cfile_name, cfile_name2))
-server_exp_2 <- drop_dir() %>% dplyr::select(name) %>% pull %>% sort
+server_exp_2 <- sort(drop_dir()$name)
 expect_identical(exp_2, server_exp_2)
 # Copy to same name, but autorename is TRUE
 file_3 <- drop_copy(cfile_name, cfile_name, autorename = TRUE)
-num_copy_files3 <- drop_dir()  %>% select(name, contains("iris-copy")) %>% nrow
+num_copy_files3 <- drop_file_count("iris-test-copy")
 expect_equal(num_copy_files3 , 3)
-
-# Copying files to folders
-# ------------------------
+drop_delete(cfile_name2)
+#
+# # Copying files to folders
+# # ------------------------
 drop_create("copy_folder")
 drop_copy(cfile_name, "copy_folder")
-dc_dir <-  drop_dir("copy_folder") %>% dplyr::select(name, contains(cfile_name)) %>% pull
-expect_equal(dc_dir, cfile_name)
-
-# Copying folders to existing folders
-# ------------------------
+dc_dir <- drop_dir("copy_folder") %>% dplyr::select(name) %>% dplyr::pull()
+expect_identical(dc_dir, cfile_name)
+drop_delete(cfile_name)
+drop_delete(file_3$metadata$path_lower)
+#
+# # Copying folders to existing folders
+# # ------------------------
 drop_create("copy_folder_2")
 drop_copy("copy_folder", "copy_folder_2")
-copy_folder_2_contents <- drop_dir("copy_folder_2/copy_folder") %>% dplyr::select(name) %>% pull
-expect_equal(copy_folder_2_contents, cfile_name)
-
-# Copying files to new folders
-# ------------------------
+copy_folder_2_contents <- drop_dir("copy_folder_2/copy_folder") %>% dplyr::select(name) %>% dplyr::pull()
+expect_identical(copy_folder_2_contents, cfile_name)
+drop_delete("copy_folder_2")
+#
+# # Copying files to new folders
+# # ------------------------
 drop_copy("copy_folder", "kerfuffle")
-d1 <- drop_dir("copy_folder")  %>% dplyr::select(name) %>% pull %>% sort
-d2 <- drop_dir("kerfuffle")  %>% dplyr::select(name) %>% pull %>% sort
+d1 <- drop_dir("copy_folder") %>% dplyr::select(name) %>% dplyr::pull() %>% sort
+d2 <- drop_dir("kerfuffle") %>% dplyr::select(name) %>% dplyr::pull() %>% sort
 expect_identical(d1, d2)
-
 drop_delete("kerfuffle")
 drop_delete("copy_folder")
-drop_delete("copy_folder_2")
-drop_delete(cfile_name)
-drop_delete(cfile_name2)
-drop_delete(file_3$metadata$path_lower)
 })
-
-# TODO
-# We need to traceless() these files and folders
-# For now, I just wanted to keep them readable
 
 # --------------------------
 # Drop Move
