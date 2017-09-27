9eab275188fd41d0ffe01ab228e9c2be131bbad8
Karthik Ram <karthik.ram@gmail.com>
2017-09-27 14:29:52 -0700

    Updated docs

--- R/drop_get_metadata.R
+++ R/drop_get_metadata.R
@@ -2,7 +2,7 @@
 #'
 #' Details vary by input and args.
 #'
-#' @param path Path to a file or folder on dropbox. Can also be an ID ("id:...") or revision ("rev:...").
+#' @param path Path to a file or folder on Dropbox. Can also be an ID ("id:...") or revision ("rev:...").
 #' @param include_media_info If TRUE, additional metadata for photo or video is returns. Defaults to FALSE.
 #' @param include_deleted If TRUE, metadata will be returned for a deleted file, otherwise error. Defaults to FALSE.
 #' @param include_has_explicit_shared_members If TRUE, the results will include a flag for each file indicating whether or not that file has any explicit members. Defaults to FALSE.
