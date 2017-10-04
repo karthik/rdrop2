5ec6800468fd7af9c8f0df3a052dba5491853362
Karthik Ram <karthik.ram@gmail.com>
2017-10-03 17:10:36 -0700

    Fix for #144

--- tests/testthat/test-02_rdrop2-upload.R
+++ tests/testthat/test-02_rdrop2-upload.R
@@ -30,19 +30,59 @@ test_that("Test that basic file ops work correctly", {
 
 test_that("Image upload works correctly", {
   skip_on_cran()
-  # This test is to see if we can upload an image (a png in this case) and make sure that it maintains file integrity.
-  # We compare hashes of local file, then the roundtrip copy.
-  dest <- traceless("rdrop2_package_test_drop.png")
-  download.file("https://www.dropbox.com/s/k61p0mvapf285cf/business_card.png?dl=0",
-                destfile = dest)
+  # This test is to see if we can upload an image (a png in this case) and make
+  # sure that it maintains file integrity. We compare hashes of local file, then
+  # the roundtrip copy.
+  # ------------------------------------------------------------------------------------------
+  # This test is failing and I have no idea why.
+  #
+  # Approach #1 -------------
+  # Leave the image in tests/testthat and reference it
+  # with testthat::test_path()
+  #
+  # dest <- testthat::test_path('rdrop2_package_test_image.png')
+  #
+  # Failure cause: Fails with file not found on Travis but works locally.
+  #   
+  #
+  # Approach #2 ------------
+  # Leave the image in tests/testthat but use rprojroot
+  # to locate/reference the file rather than using test_path
+  #
+  # dest <- rprojroot::find_testthat_root_file('rdrop2_package_test_image.png')
+  #
+  # Failure cause: Fails with file not found on Travis but works locally.
+  #
+  # Approach #3 ------------------
+  # Leave the image in `inst/extdata`, then
+  # upload from there. Works locally but fails on Travis.
+  #
+  # image_path <- system.file("extdata", package="rdrop2")
+  # image_name <- "rdrop2_package_test_image.png"
+  # dest <- file.path(image_path, image_name)
+  #  ¯\_(ツ)_/¯
+  # ------------------------------------------------------------------------------------------
+  # First locate the png inside the package. This works fine locally with all
+  # three approaches but fails on Travis with all 3 approaches
+  dest <- testthat::test_path('rdrop2_package_test_image.png')
+  # No point continuing if you can’t find the file
+  expect_true(file.exists(dest))
+  # Hash the file locally
   local_file_hash <- digest::digest(dest)
+  # Upload it to Dropbox. The test fails here in the assertion inside the function
+  # that makes sure the file exists before uploading
   drop_upload(dest)
+  # Now delete the local copy
   unlink(dest)
-  drop_download(dest)
+  # Now download the file again, but make sure it's back in the testthat/tests directoryQ
+  drop_download(path = basename(dest), local_path = testthat::test_path("."))
+  # Compute the hash on the return file
   roundtrip_file_hash <-  digest::digest(dest)
+  # Are they the same?
   expect_equal(local_file_hash, roundtrip_file_hash)
+  # Delete the Dropbox copy
   drop_delete(dest)
-  unlink(dest)
+  # Only delete this copy if it is copied from extdata, otherwise we'll lose the only copy.
 })
 
 # Test upload of a non existent file
