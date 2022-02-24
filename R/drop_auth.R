a67c9eea0a976e07e00e05104517629ece85bb1b
Jean-Claude Arbaut <jcarbaut@hotmail.fr>
2022-02-24 23:27:54 +0100

    add query_authorize_extra optional parameter to drop_auth in order to allow passing token_access_type = 'offline') to httr::oauth2.0_token

--- R/drop_auth.R
+++ R/drop_auth.R
@@ -55,7 +55,8 @@ drop_auth <- function(new_user = FALSE,
                       key = "mmhfsybffdom42w",
                       secret = "l8zeqqqgm1ne5z0",
                       cache = TRUE,
-                      rdstoken = NA) {
+                      rdstoken = NA,
+                      query_authorize_extra = list()) {
 
   # check if token file exists & use it
   if (new_user == FALSE &  !is.na(rdstoken)) {
@@ -86,7 +87,8 @@ drop_auth <- function(new_user = FALSE,
     dropbox_app <- httr::oauth_app("dropbox", key, secret)
 
     # get the token
-    dropbox_token <- httr::oauth2.0_token(dropbox, dropbox_app, cache = cache)
+    dropbox_token <- httr::oauth2.0_token(dropbox, dropbox_app, cache = cache,
+                                          query_authorize_extra = query_authorize_extra)
 
     # make sure we got a token
     if (!inherits(dropbox_token, "Token2.0")) {
